471555c74c857e9655a3914918d26468d609a120
Zohaib Shaik <shaikzohaibgec@gmail.com>
2024-10-25 14:20:28 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Personal Portfolio Website built with React v16
+![image](https://github.com/warisamir/Portfolio/assets/77074869/bd93e7b7-1d72-4dee-ba1a-d0bcb47e1be2)
+
+
+A clean and customizable ReactJS portfolio template for fellow developers. Credits to HamishMW!. View demo at [https://http://vercel.com//]
+
+-   built using [React](https://reactjs.org)
+-   bootstrapped with [Create React App](https://github.com/facebook/create-react-app)
+-   styled with [Material-UI](https://material-ui.com)
+-   deployed on vercel [Vercel](https://vercel.com)(https://portfolio-fqac2euei-warisamir.vercel.app/)
+
+Special thanks to [Jo Lienhoop](https://github.com/JoHoop), and [Hamish Williams](https://github.com/HamishMW) for the inspiration.
+
+## Features
+
+-   All of the personal information is populated from the resume.json file following the [JSON Resume](https://jsonresume.org) standard, a community driven open source initiative to create a JSON based standard for resumes. Discover the official schema [here](https://jsonresume.org/schema).
+-   The toggle/switch for the dark mode syncs its state to the local storage.
+
+#### Coming soon
+
+-   Two beautiful resume page templates generated based on the data in the resume.json file -- a modern approach of the traditional printed CV.
+-   Rich Google search results using structured data with [json-ld.org/](https://json-ld.org).
+
+## Customization
+
+Feel free to fork this project and customize it with your own information and style.
+
+Refer to the [Material UI docs](https://material-ui.com/customization/theming) for guidance on how to quickly customize the themes, components and colors to suit your tastes.
+
+If you improve the app in any way a pull request would be very much appreciated ✌️
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm install`
+
+to install the dependencies.
+
+### `npm start`
+
+to run the app in the development mode at [http://localhost:3000](http://localhost:3000)<br />
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br />
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br />
+Your app is ready to be deployed!
---
+++ src/components/works/Works.js
@@ -0,0 +1,102 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { useState } from "react";
+import { Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { TextDecrypt } from "../content/TextDecrypt";
+
+import './Works.css';
+
+// Import ../../assets/recentprojects/
+import Portfolio from '../../assets/recentprojects/react-portfolio.png';
+import Veritru from '../../assets/recentprojects/veritru.png';
+import Lofo from '../../assets/recentprojects/lofo.png';
+import Startup from '../../assets/recentprojects/startup.png';
+import Lacalle from '../../assets/recentprojects/lacalle.png';
+
+const useStyles = makeStyles((theme) => ({
+  main: {
+    maxWidth: '100vw',
+    marginTop: '3em',
+    marginBottom: "auto",
+  },
+}));
+
+export const Works = () => {
+  const classes = useStyles();
+  const [projects, setProjects] = useState([
+    { 
+      id: 1,
+      title: 'React Portfolio', 
+      description: `Designed and developed a ReactJS portfolio 
+      with fancy 3D animations using Three.js for 
+      the background element.`,
+      alter: 'React Portfolio',
+      image: `${Portfolio}`,
+      url: `https://portfolio-git-main-warisamirs-projects.vercel.app/`
+    },
+    { 
+      id: 2,
+      title: 'Docs clone Project', 
+      description: `An textor project website built using
+       simple react to design and develop its frontend.`,
+      alter: 'Google docs  Project',
+      image: `${Veritru}`,
+      url: `https://google-docs-clone-gray-two.vercel.app/#/`
+    },
+    { 
+      id: 3,
+      title: 'Web-Socket Chat and Meeting Application', 
+      description: `It is a chat application project
+       built using ReactJS and Express with Socket.io to 
+        demonstrate the realtime chat capabilities and having a meeting of 4 member can easily setup for a quick discussion & everyone can share their screen. `,
+      alter: 'CometChat Project',
+      image: `${Lofo}`,
+      url:`https://discord-ak-clone.netlify.app/`
+    },
+    { 
+      id: 4,
+      title: 'SpotifyRed Project', 
+      description: `A simple react app that uses the Youtube
+       API to listen and watch songs and videos online 
+       from anywhere in the world without ads.`,
+      alter: 'Youtube Project',
+      image: `${Startup}`,
+      url:`https://youtube-project-eight.vercel.app/`
+    },
+    { 
+      id: 5,
+      title: 'FoodApp Delivery Project', 
+      description: `A website project for the Food Delivery service
+       as the online food ordering and have monthly or 
+       annual subsicrption according to the user need. but a needed a quick AWS server to setup docker environment to run.`,
+      alter: 'Food Project',
+      image: `${Lacalle}`,
+      url:`https://github.com/warisamir/FoodApp`
+    },
+  ]);
+      return (
+        <section id="works">
+          <Container component="main" className={classes.main} maxWidth="md">
+            {projects.map((project) => (
+              <div className="project" key={project.id}>
+                <div className="__img_wrapper">
+                  <img src={project.image} alt={project.alter} />
+                  <a href={project.url} target="_blank" rel="noopener noreferrer" className="hidden-link">
+                    <span className="hidden-text">Visit project</span>
+                  </a>
+                </div>
+                <div className="__content_wrapper">
+                  <h3 className="title">
+                    <TextDecrypt text={project.id + '. ' + project.title} />
+                  </h3>
+                  <p className="description">
+                    {project.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </Container>
+        </section>
+      );  
+};
